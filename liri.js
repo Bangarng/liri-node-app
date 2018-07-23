dfa67fdb5c9e3a4e0b751cbb3f0b58073a9a6d6c
Dickie Stevens <rsk0528@gmail.com>
2018-07-23 08:22:33 -0400

    semi working liri

--- liri.js
+++ liri.js
@@ -1,1 +1,37 @@
-require("dotenv").config();
+//get dem keys
+var keys = require("./keys.js");
+//get dem twitters
+var Twitter = require('twitter');
+//get dem spotifies
+var Spotify = require('node-spotify-api');
+//get dem requests
+var request = require('request');
+//get the fs
+var fs = require("fs");
+
+var getTweets = function() {
+    var client = new Twitter(keys.Twitter);
+    var params = {
+        screen_name: 'BTMComic'
+    };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        }
+    });
+}
+
+var getArtistNames = function(artist) {
+    return artist.name;
+}
+
+var getSpotify = function() {
+    var spotify = new Spotify(keys.Spotify);    
+    spotify.search({ type: 'track', query: 'Hit me baby one more' }, function(err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+    
+    console.log(JSON.stringify(data,null, 2)); 
+    });
+}
