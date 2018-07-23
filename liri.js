80c4b113cb44ea65c4a4716a683e33109824e840
Dickie Stevens <rsk0528@gmail.com>
2018-07-23 09:25:52 -0400

    polishing out

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//require("dotenv").config();
 //get dem keys
 var keys = require("./keys.js");
 //get dem twitters
@@ -9,55 +10,51 @@ var request = require('request');
 //get the fs
 var fs = require("fs");
 
-// FUNCTIONS
-// =====================================
-// Writes to the log.txt file
-var getArtistNames = function(artist) {
-    return artist.name;
-  };
-  // Function for running a Spotify search
-  var getSpotify = function(songName) {
+//Get Tweets
+var getTweets = function() {
+    var client = new Twitter(keys.twitterKeys);
+    var params = {
+        screen_name: "BTMComic"
+    };
+    client.get("statuses/user_timeline", params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log("");
+                console.log(tweets[i].text);
+            }
+        }
+    });
+};
+  
+// Get Spotify
+var getSpotify = function(songName) {
     if (songName === undefined) {
-      songName = "What's my age again";
+    songName = "What's my age again";
     }
     spotify.search(
-      {
+    {
         type: "track",
         query: songName
-      },
-      function(err, data) {
+    },
+    function(err, data) {
         if (err) {
-          console.log("Error occurred: " + err);
-          return;
+        console.log("Error occurred: " + err);
+        return;
         }
         var songs = data.tracks.items;
         for (var i = 0; i < songs.length; i++) {
-          console.log(i);
-          console.log("artist(s): " + songs[i].artists.map(getArtistNames));
-          console.log("song name: " + songs[i].name);
-          console.log("preview song: " + songs[i].preview_url);
-          console.log("album: " + songs[i].album.name);
-          console.log("-----------------------------------");
+        console.log(i);
+        console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+        console.log("song name: " + songs[i].name);
+        console.log("preview song: " + songs[i].preview_url);
+        console.log("album: " + songs[i].album.name);
+        console.log("-----------------------------------");
         }
-      }
+    }
     );
-  };
-  // Function for running a Twitter Search
-  var getTweets = function() {
-    var client = new Twitter(keys.twitterKeys);
-    var params = {
-      screen_name: "kiddycads"
-    };
-    client.get("statuses/user_timeline", params, function(error, tweets, response) {
-      if (!error) {
-        for (var i = 0; i < tweets.length; i++) {
-          console.log(tweets[i].created_at);
-          console.log("");
-          console.log(tweets[i].text);
-        }
-      }
-    });
-  };
+};
+
   // Function for running a Movie Search
   var getMovie = function(movieName) {
     if (movieName === undefined) {
